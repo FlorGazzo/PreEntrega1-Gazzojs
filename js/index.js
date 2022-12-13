419e52dd7e0e7c9f0c2300810801bfcfbaf7013d
FlorGazzo <gazzoflorencia.pa@gmail.com>
2022-12-12 23:16:30 -0300

    agrego prompt y alert

--- js/index.js
+++ js/index.js
@@ -1,34 +1,51 @@
-let holidaysCalc = document.getElementsByClassName ("holydaysCalc")
-    holidaysCalc.addEventListener ("submit", calcExpenses)
+let entrada = prompt("¿Su destino será en el interior o exterior?");
 
-function getValues(){
-    let destiny = document.getElementsByClassName ("destiny").value,
-        budget = document.getElementsByClassName ("budget").value,
-        accomodation = document.getElementsByClassName ("accomdation").value,
-        transport = document.getElementsByClassName ("transport").value,
-        food = document.getElementsByClassName ("food").value,
-        extra = document.getElementsByClassName ("extra").value;
+while (entrada != "ESC") {
+    switch (entrada) {
+        case "interior":
+            alert("Bienvenido/a, a continuación podrá calcular su viaje dentro del país")
+            break;
 
-    return {destiny, budget, accomodation, transport, food, extra}
+        case "exterior":
+            alert("Bienvenido/a, a continuación podrá calcular su viaje fuera del país")
+            break;
+
+        default:
+            alert("ingrese la palabra dentro, si desea realizar su viaje en el país o fuera si desea realizar su viaje en el exterior")
+            break;
+    }
+
+    let holidaysCalc = document.getElementsByClassName("holydaysCalc")
+    holidaysCalc.addEventListener("submit", calcExpenses)
+
+    function getValues() {
+        let destiny = document.getElementsByClassName("destiny").value,
+            budget = document.getElementsByClassName("budget").value,
+            accomodation = document.getElementsByClassName("accomdation").value,
+            transport = document.getElementsByClassName("transport").value,
+            food = document.getElementsByClassName("food").value,
+            extra = document.getElementsByClassName("extra").value;
+
+        return { destiny, budget, accomodation, transport, food, extra }
     }
 
 
-function calcExpenses (e){
-    e.preventDefault();
+    function calcExpenses(e) {
+        e.preventDefault();
 
-    const {destiny, budget, accomodation, transport, food, extra} = getValues();
+        const { destiny, budget, accomodation, transport, food, extra } = getValues();
 
-    let expenses = parseInt(accomodation) + parseInt(transport) + parseInt(food) + parseInt(extra)
-    let balance = budget - expenses
+        let expenses = parseInt(accomodation) + parseInt(transport) + parseInt(food) + parseInt(extra)
+        let balance = budget - expenses
 
-    UI(destiny, budget, balance);
-}
+        UI(destiny, budget, balance);
+    }
 
-function UI (destiny, budget, balance) {
-    let result = document.getElementsByClassName ("result")
-    let dataPrint = document.createElement ("div")
+    function UI(destiny, budget, balance) {
+        let result = document.getElementsByClassName("result")
+        let dataPrint = document.createElement("div")
 
-    dataPrint.innerHTML =`
+        dataPrint.innerHTML = `
         <div class= "row">
             <div class="col s4">
                 <h6>${destiny}</h6>
@@ -42,12 +59,14 @@ function UI (destiny, budget, balance) {
         </div>
     `
 
-    result.appendChild(dataPrint)
+        result.appendChild(dataPrint)
 
-    reset();
+        reset();
 
-}
+    }
 
-function reset() {
-    document.getElementsByClassName ("holydaysCalc").reset()
+    function reset() {
+        document.getElementsByClassName("holydaysCalc").reset()
+    }
+
 }
