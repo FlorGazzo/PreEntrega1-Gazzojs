59f2b58528c53043a8e6588db2f0899cea04a0c8
FlorGazzo <gazzoflorencia.pa@gmail.com>
2022-12-20 13:29:50 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
-let entrada = prompt("¿Su destino será en el interior o exterior?");
+let entrada = prompt("¡Bienvenido/a a Calculá tu viaje! ¿Su destino será en el interior o exterior?");
 
 while (entrada != "ESC") {
     switch (entrada) {
         case "interior":
-            alert("Bienvenido/a, a continuación podrá calcular su viaje dentro del país")
+            alert("Bienvenido/a, a continuación podrá calcular su viaje en el interior")
             break;
 
         case "exterior":
-            alert("Bienvenido/a, a continuación podrá calcular su viaje fuera del país")
+            alert("Bienvenido/a, a continuación podrá calcular su viaje en el exterior")
             break;
 
         default:
-            alert("ingrese la palabra dentro, si desea realizar su viaje en el país o fuera si desea realizar su viaje en el exterior")
+            alert("ingrese la palabra INTERIOR, si desea realizar su viaje en el país o EXTERIOR si desea realizar su viaje en el exterior")
             break;
     }
 
-    let holidaysCalc = document.getElementsByClassName("holydaysCalc")
+    alert ("Comenzemos")
+
+let holidaysCalc = document.getElementsByClassName("holydaysCalc")
     holidaysCalc.addEventListener("submit", calcExpenses)
 
     function getValues() {
-        let destiny = document.getElementsByClassName("destiny").value,
-            budget = document.getElementsByClassName("budget").value,
-            accomodation = document.getElementsByClassName("accomdation").value,
-            transport = document.getElementsByClassName("transport").value,
-            food = document.getElementsByClassName("food").value,
-            extra = document.getElementsByClassName("extra").value;
+        let destiny = document.getElementsByClassName("destiny").value;
+        let budget = document.getElementsByClassName("budget").value;
+        let accomodation = document.getElementsByClassName("accomdation").value;
+        let transport = document.getElementsByClassName("transport").value;
+        let food = document.getElementsByClassName("food").value;
+        let extra = document.getElementsByClassName("extra").value;
 
         return { destiny, budget, accomodation, transport, food, extra }
     }
@@ -46,18 +48,18 @@ while (entrada != "ESC") {
         let dataPrint = document.createElement("div")
 
         dataPrint.innerHTML = `
-        <div class= "row">
-            <div class="col s4">
-                <h6>${destiny}</h6>
+            <div class= "row">
+                <div class="col s4">
+                    <h6>${destiny}</h6>
+                </div>
+                <div class="col s4">
+                    <h6>${budget}</h6>
+                </div>
+                <div class="col s4">
+                    <h6>${balance}</h6>
+                </div>
             </div>
-            <div class="col s4">
-                <h6>${budget}</h6>
-            </div>
-            <div class="col s4">
-                <h6>${balance}</h6>
-            </div>
-        </div>
-    `
+        `
 
         result.appendChild(dataPrint)
 
@@ -68,5 +70,4 @@ while (entrada != "ESC") {
     function reset() {
         document.getElementsByClassName("holydaysCalc").reset()
     }
-
 }
