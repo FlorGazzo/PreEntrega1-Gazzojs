f37e38792872a963ceb60fea953667e3a436f52d
FlorGazzo <gazzoflorencia.pa@gmail.com>
2022-12-29 20:43:12 -0300

    localStorage y JSON

--- js/index.js
+++ js/index.js
@@ -1,57 +1,54 @@
-let entrada = prompt("¡Bienvenido/a a Calculá tu viaje! ¿Su destino será en el interior o exterior?");
 let holidaysCalc = document.getElementById("holidaysCalc");
-let flag;
 
-do{
-    switch (entrada) {
-        case "interior":
-            alert("Bienvenido/a, a continuación podrá calcular su viaje en el interior")
-            alert ("Comenzemos")
-            flag = false;
-            break;
-        case "exterior":
-            alert("Bienvenido/a, a continuación podrá calcular su viaje en el exterior")
-            alert ("Comenzemos")
-            flag = false;
-            break;
-        default:
-            alert("ingrese la palabra INTERIOR, si desea realizar su viaje en el país o EXTERIOR si desea realizar su viaje en el exterior")
-            break;
-    } 
-}while (flag) {
+holidaysCalc.addEventListener("submit", (e) => {
+    e.preventDefault();
+    calcExpenses()
+})
 
-    holidaysCalc.addEventListener("submit", (e) => {
-        e.preventDefault();
-        calcExpenses()
-    } )
+function getValues() {
+    let destiny = document.getElementById("destiny").value;
+    let budget = document.getElementById("budget").value;
+    let accomodation = document.getElementById("accomodation").value;
+    let transport = document.getElementById("transport").value;
+    let food = document.getElementById("food").value;
+    let extra = document.getElementById("extra").value;
 
-    function getValues() {
-        let destiny = document.getElementById("destiny").value;
-        let budget = document.getElementById("budget").value;
-        let accomodation = document.getElementById("accomodation").value;
-        let transport = document.getElementById("transport").value;
-        let food = document.getElementById("food").value;
-        let extra = document.getElementById("extra").value;
+    return { destiny, budget, accomodation, transport, food, extra }
+}
 
-        return { destiny, budget, accomodation, transport, food, extra }
-    }
+localStorage.setItem("destiny", JSON.stringify(destiny));
+const destinyInLocalStorage = JSON.parse(localStorage.getItem("destiny"))
 
+localStorage.setItem("budget", JSON.stringify(budget));
+const budgetInLocalStorage = JSON.parse(localStorage.getItem("budget"))
 
-    function calcExpenses() {
+localStorage.setItem("accomodation", JSON.stringify(accomodation));
+const accomodationInLocalStorage = JSON.parse(localStorage.getItem("accomodation"))
 
-        const { destiny, budget, accomodation, transport, food, extra } = getValues();
+localStorage.setItem("transport", JSON.stringify(transport));
+const transportInLocalStorage = JSON.parse(localStorage.getItem("accomodation"))
 
-        let expenses = parseInt(accomodation) + parseInt(transport) + parseInt(food) + parseInt(extra)
-        let balance = budget - expenses
+localStorage.setItem("food", JSON.stringify(food));
+const foodInLocalStorage = JSON.parse(localStorage.getItem("food"))
 
-        UI(destiny, budget, balance);
-    }
+localStorage.setItem("extra", JSON.stringify(extra));
+const extraInLocalStorage = JSON.parse(localStorage.getItem("extra"))
 
-    function UI(destiny, budget, balance) {
-        let result = document.getElementById("result")
-        let dataPrint = document.createElement("div")
+function calcExpenses() {
 
-        dataPrint.innerHTML = `
+    const { destiny, budget, accomodation, transport, food, extra } = getValues();
+
+    let expenses = parseInt(accomodation) + parseInt(transport) + parseInt(food) + parseInt(extra)
+    let balance = budget - expenses
+
+    UI(destiny, budget, balance);
+}
+
+function UI(destiny, budget, balance) {
+    let result = document.getElementById("result")
+    let dataPrint = document.createElement("div")
+
+    dataPrint.innerHTML = `
             <div class= "row">
                 <div class="col s4">
                     <h6>${destiny}</h6>
@@ -65,13 +62,13 @@ do{
             </div>
         `
 
-        result.appendChild(dataPrint)
+    result.appendChild(dataPrint)
 
-        reset();
+    reset();
 
-    }
-
-    function reset() {
-        holidaysCalc.reset()
-    }
 }
+
+function reset() {
+    holidaysCalc.reset()
+}
+
