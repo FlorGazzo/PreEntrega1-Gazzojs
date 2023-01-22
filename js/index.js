473e3ff5fbc2e3717820d87aa8dc5531ee1b0708
FlorGazzo <gazzoflorencia.pa@gmail.com>
2023-01-22 19:26:02 -0300

    se realizaron correcciones en el archivo css y se agrego metodo fetch

--- js/index.js
+++ js/index.js
@@ -72,3 +72,28 @@ function reset() {
     holidaysCalc.reset()
 }
 
+function balanceColours() {
+
+    const { balance } = getValues()
+
+
+    if (balance < 0) {
+        document.getElementById('balance').classList.add('red')
+    }
+    else if (balance > 0) {
+        document.getElementById('balance').classList.add('green')
+    }
+}
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
